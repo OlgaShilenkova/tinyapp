a9b286eadf1e93dca621898f973e6fd396ef7c69
Olga Shilenkova <Olga1shilenkova@gmail.com>
2021-12-06 23:41:00 -0600

    after receiving post request,generate short url, save shorturl and long url to object, redirect to short  url path

--- express_server.js
+++ express_server.js
@@ -2,13 +2,26 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
-// function generateRandomString() {
+function generateRandomString() {
 
-// }
+  let nums = [];
+  for (let i = 0; i < 3; i++) {
+    let num = Math.floor(Math.random() * 9);
+    nums.push(num);
+  }
+  const alphabet = "abcdefghijklmnopqrstuvwxyz"
+  for (let i = 0; i < 3; i++) {
+    let randomNum = Math.floor(Math.random() * alphabet.length);
+    let character = alphabet[randomNum];
+    nums.push(character);
+  }
+  return nums.join("");
+
+}
 
 
 const urlDatabase = {
@@ -20,22 +33,29 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
 
 app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);``
+  res.json(urlDatabase); ``
 });
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL:urlDatabase[req.params.shortURL]
-   };
+  const templateVars = {
+    shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]
+  };
   res.render("urls_show", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  // const longURL = ...
+  res.redirect(longURL);
 });
 
 app.get("/hello", (req, res) => {
@@ -43,8 +63,11 @@ app.get("/hello", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  let smallUrl = generateRandomString();
+  const newLongUrl = req.body.longURL;
+  urlDatabase[smallUrl] = newLongUrl;
+  res.redirect("/urls/"+ smallUrl);
+  // res.send(urlDatabase.tinyUrl = "longURL")
 });
 
 app.listen(PORT, () => {
