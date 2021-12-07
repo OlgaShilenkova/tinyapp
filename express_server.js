9422522f7efee9e246537e36c7a8e98c3ef67819
Olga Shilenkova <Olga1shilenkova@gmail.com>
2021-12-07 17:14:17 -0600

    Added delete button and rout to serve it

--- express_server.js
+++ express_server.js
@@ -2,12 +2,16 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
-function generateRandomString() {
+//
+// MIDDLEWARE
+//
+app.use(bodyParser.urlencoded({ extended: true }));
 
+// Function to generate short Url
+function generateRandomString() {
   let nums = [];
   for (let i = 0; i < 3; i++) {
     let num = Math.floor(Math.random() * 9);
@@ -20,7 +24,6 @@ function generateRandomString() {
     nums.push(character);
   }
   return nums.join("");
-
 }
 
 
@@ -29,39 +32,31 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+//
+//ROUTES
+//
+
+//
+//READ
+//
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase); ``
 });
+
+//
+// (show all the url links)
+//
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
-});
-
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
-
-app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = {
-    shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]
-  };
-  res.render("urls_show", templateVars);
-});
-//for redirect links from short urls pages
-app.get("/u/:shortURL", (req, res) => {
-  console.log(req.params);
-   const longURL = urlDatabase[req.params.shortURL]
-   console.log("longURL",longURL);
-   res.redirect(longURL);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.post("/urls", (req, res) => {
@@ -70,6 +65,46 @@ app.post("/urls", (req, res) => {
   urlDatabase[smallUrl] = newLongUrl;
   res.redirect("/urls/"+ smallUrl);
 });
+
+//
+// ? CREATE new
+//
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+//
+// ? UPDATE
+//
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = {
+    shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]
+  };
+  res.render("urls_show", templateVars);
+});
+
+//
+//for redirect links from short urls pages
+//
+app.get("/u/:shortURL", (req, res) => {
+  console.log(req.params);
+   const longURL = urlDatabase[req.params.shortURL]
+   console.log("longURL",longURL);
+   res.redirect(longURL);
+});
+
+//
+// DELETE
+//
+
+app.post( "/urls/:shortURL/delete", (req,res)=>{
+  const idToDelete = req.params.shortURL;
+  delete urlDatabase[idToDelete];
+  res.redirect("/urls")
+
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
